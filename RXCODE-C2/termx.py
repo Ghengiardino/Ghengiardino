24f5d936f680e7533a2ccf65fe17db294a3d5164
RXCODE-C2 <133853001+Ghengiardino@users.noreply.github.com>
2023-08-23 10:33:17 +0700

    Lib

---
+++ RXCODE-C2/termx.py
@@ -0,0 +1,19 @@
+import os
+import time
+import sys
+# Set color
+G = '\033[1;32m' # Green
+
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.002)
+delay_print
+delay_print (""+G+"  ████████╗██████╗ ██████╗ ███╗   ███╗██╗   ██╗██╗  ██╗""\n")
+delay_print (""+G+"  ╚══██╔══╝╚═══██║ ██╔══██╗████╗ ████║██║   ██║╚██╗██╔╝""\n")
+delay_print (""+G+"     ██║    █████║ ██████╔╝██╔████╔██║██║   ██║ ╚███╔╝""\n")
+delay_print (""+G+"     ██║    ╚══██║ ██╔══██╗██║╚██╔╝██║██║   ██║ ██╔██╗""\n")
+delay_print (""+G+"     ██║   ██████║ ██║  ██║██║ ╚═╝ ██║╚██████╔╝██╔╝ ██╗""\n")
+delay_print (""+G+"     ╚═╝   ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝""\n")
+delay_print (""+G+" <═══════════════Created By 'RXCODE-C2'═══════════════>""\n")
